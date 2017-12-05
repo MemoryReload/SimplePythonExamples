3819d97eb3d3859fdce6bc9bda8fcecea43d4c60
MemoryReload <heping_tsdwx@163.com>
2017-12-05 18:19:52 +0800

    add ex15.py file

--- ex25.py
+++ ex25.py
@@ -1,4 +1,4 @@
-def break_wordw(stuff):
+def break_words(stuff):
     """"This function will break up words for us."""
     words = stuff.split(' ')
     return words
@@ -20,4 +20,18 @@ def print_last_word(words):
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words."""
     words = break_words(sentence)
-    
+    return sort_words(words)
+
+def print_first_and_last(sentence):
+    """Prints the first and last words of the sentence."""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+    """Sorts the words then prints the first and last one."""
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+
