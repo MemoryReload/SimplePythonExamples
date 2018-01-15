4475877c05a652f03ad94c07692832ec64ba4459
MemoryReload <heping_tsdwx@163.com>
2018-01-15 10:44:58 +0800

    modify Example1_25

---
+++ Example/Example1_25.py
@@ -0,0 +1,21 @@
+import os
+def open(filename, mode="rb"):
+    import __builtin__
+    file = __builtin__.open(filename, mode)
+    if file.read(5) not in ("GIF87", "GIF89"):
+        raise IOError, "not a GIF file"
+    file.seek(0)
+    return file
+
+if __name__ == "__main__":
+    while True:
+        file = raw_input("Please enter the gif file Path:\n")
+        file = os.path.expanduser(file)
+        if not os.path.isabs(file):
+            file = os.path.abspath(file)
+        if os.path.exists(file) and os.path.isfile(file):
+            fp = open(file)
+            print  len(fp.read()), "bytes"
+            break
+        else:
+            print("the file path is not correct!")
