2a79d536b0a4a9ff8ff57154ceb7961e5a5571e2
Minimalist <heping_tsdwx@163.com>
2018-01-26 21:21:58 +0800

    final modification for Example1_48.py

--- Example/Example1_48.py
+++ Example/Example1_48.py
@@ -1,30 +1,33 @@
-import  os
+import os
+import re
+
 
 class DirectoryWalker(object):
-# a forward iterator that traverses a directory tree
+    # a forward iterator that traverses a directory tree
 
-def __init__(self, directory):
-    self.stack = [directory]
-    self.files = []
-    self.index = 0
+    def __init__(self, directory):
+        self.stack = [directory]
+        self.files = []
+        self.index = 0
 
+    def __getitem__(self, index):
+        while 1:
+            try:
+                file = self.files[self.index]
+                self.index += 1
+            except IndexError:
+                # pop next directory from stack
+                self.directory = self.stack.pop()
+                self.files = os.listdir(self.directory)
+                self.index = 0
+            else:
+                # got a filename
+                fullname = os.path.join(self.directory, file)
+                if os.path.isdir(fullname) and not os.path.islink(fullname) and not re.match("^\.", file, re.MULTILINE):
+                    self.stack.append(fullname)
+                elif os.path.isfile(fullname):
+                    return fullname
 
-def __getitem__(self, index):
-    while 1:
-        try:
-            file = self.files[self.index]
-            self.index += 1
-        except IndexError:
-            #pop next directory from stack
-            self.directory = self.stack.pop()
-            self.files = os.listdir(self.directory)
-            self.index = 0
-        else:
-            #got a filename
-            fullname = os.path.join(self.directory, file)
-            if os.path.isdir(fullname) and not os.path.islink(fullname):
-                self.stack.append(fullname)
-            return fullname
 
 if __name__ == "__main__":
     for file in DirectoryWalker("."):
