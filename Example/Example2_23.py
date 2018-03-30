2e368bad92ac7439e28c5ef615589684bb69b4f1
MemoryReload <heping_tsdwx@163.com>
2018-03-30 19:23:10 +0800

    more example code

--- Example/Example2_23.py
+++ Example/Example2_23.py
@@ -13,27 +13,27 @@ try:
 except getopt.GetoptError, (error, message):
     print "Error code: "+error+" message: "+message
     help_descrition()
-else:
-    if not args:
+    exit(1)
+if not args:
+    help_descrition()
+    exit(1)
+for (opt, val) in options:
+    if opt in ["-h", "--help"]:
         help_descrition()
         exit(0)
-    for (opt, val) in options:
-        if opt in ["-h", "--help"]:
-            help_descrition()
-            exit(0)
-        elif opt in ["-v", "--verbose"]:
-            verbose = True
-        elif opt in ["-t", "--type"]:
-            file_type = val
-        elif opt in ["-o", "--output"]:
-            output = val
-        else:
-            pass
+    elif opt in ["-v", "--verbose"]:
+        verbose = True
+    elif opt in ["-t", "--type"]:
+        file_type = val
+    elif opt in ["-o", "--output"]:
+        output = val
+    else:
+        pass
 
-    print "verbose: "+repr(verbose)
-    print "file_type: "+file_type
-    print "output_file: "+output
-    print "input_file"+repr(args)
+print "verbose: "+repr(verbose)
+print "file_type: "+file_type
+print "output_file: "+output
+print "input_file"+repr(args)
 
 
 
